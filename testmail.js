36772d1cf8a47abe69798f625aca25aa8885e804
Michael <michael.dean.haynie@gmail.com>
2017-08-30 19:43:45 -0700

    Got a test email working!

---
+++ testmail.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+// console.log(process.env.PRIVATEEMAIL_USER);
+// console.log(process.env.PRIVATEEMAIL_PASS);
+
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  host: 'mail.privateemail.com',
+  port: 587,
+  auth: {
+    user: process.env.PRIVATEEMAIL_USER,
+    pass: process.env.PRIVATEEMAIL_PASS, 
+  }
+});
+
+var mailOptions = {
+  from: process.env.PRIVATEEMAIL_USER,
+  to: 'michael.dean.haynie@gmail.com',
+  subject: 'This is the subject of a test email (587)',
+  text: 'Hi!',
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    };
+});
